27a94582ff532ae2f7959e493f28b09fde29db1e
TR0J3N <95092220+TR0J3N@users.noreply.github.com>
2021-11-26 22:28:22 +0530

    Add files via upload

---
+++ Yukinon/modules/helper_funcs/moretools.py
@@ -0,0 +1,16 @@
+__help__ = """
+*Text To Speech:*
+⦁ `/tts <text>`*:* convert text to speech.
+*Zipper:*
+⦁ `/zip`*:* reply to a telegram file to compress it in .zip format.
+⦁ `/unzip`*:* reply to a telegram file to decompress it from the .zip format.
+*Zombies:*
+⦁ `/zombies`*:* Check if any deleted accounts available in the group member list and give results.
+*Stickers:*
+⦁ `/stickerid`*:* reply to a sticker to find its file ID.
+⦁ `/getsticker`*:* reply to a sticker to upload its raw PNG file.
+⦁ `/kang`*:* reply to a sticker to add it to your pack.
+⦁ `/stickers`*:* Find stickers for given term.
+"""
+
+__mod_name__ = "Tools"
