ebc34bb5b009d6933c6c48b5d73cfb180a52436c
TR0J3N <95092220+TR0J3N@users.noreply.github.com>
2021-11-27 11:18:23 +0530

    Add files via upload

---
+++ Yukinon/modules/about.py
@@ -0,0 +1,11 @@
+__help__ = """
+** 😍 I'm Yukinoshita Yukino - A Cute And OP Telegram Group Manager 🥰 **
+
+Boyfriend❤ [TROJ3N](https://t.me/tr0j3n)
+
+Friend💛 [Eren Yeager](https://t.me/Peaceful_wolf_016)
+
+Other Girls💕 [Waifu Net](https://t.me/waifuNetwork)
+
+"""
+__mod_name__ = "About ⚡"
